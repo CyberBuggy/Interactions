74ba608c1b26178789f23090e6c137daa144aa22
KaitoMajima <68963406+KaitoMajima@users.noreply.github.com>
2022-04-18 00:45:43 -0300

    Remove this. keywords from PlayerInteractions.cs

--- Scripts/PlayerInteractions.cs
+++ Scripts/PlayerInteractions.cs
@@ -10,12 +10,12 @@ namespace CyberBuggy
         private IInteractable _currentInteractable;
         private void OnTriggerEnter2D(Collider2D other)
         {
-            this._currentInteractable = other.GetComponent<IInteractable>();
+            _currentInteractable = other.GetComponent<IInteractable>();
         }
         private void OnTriggerExit2D(Collider2D other)
         {
-            if (other.GetComponent<IInteractable>() == this._currentInteractable)
-                this._currentInteractable = null;
+            if (other.GetComponent<IInteractable>() == _currentInteractable)
+                _currentInteractable = null;
         }
 
         private void OnInteract()
